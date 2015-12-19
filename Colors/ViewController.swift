ae51eb5b9bcc8e36e1e1f7e809ec48a7188a8bd8
tanakaworld <yutaro.tanaka.world@gmail.com>
2015-12-19 18:22:12 +0900

    Initial commit

---
+++ Colors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Colors
+//
+//  Created by me on 12/19/15.
+//  Copyright © 2015 YutaroTanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
