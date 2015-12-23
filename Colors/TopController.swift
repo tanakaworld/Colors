0456943a2387e9a0c0b56b18afb4e0700c2577db
tanakaworld <yutaro.tanaka.world@gmail.com>
2015-12-23 16:25:43 +0900

    Replace top viewcontroller

--- Colors/TopController.swift
+++ Colors/TopController.swift
@@ -2,107 +2,34 @@
 //  TopController.swift
 //  Colors
 //
-//  Created by me on 12/19/15.
+//  Created by me on 12/23/15.
 //  Copyright © 2015 YutaroTanaka. All rights reserved.
 //
 
 import UIKit
 
-class TopController: UIViewController, UIPageViewControllerDataSource {
-    
-    var pageViewController: UIPageViewController!
-    var pageTitles: NSArray!
-    var pageIndexes: NSArray!
-    var colors: Array<UIColor>!
-    
+class TopController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // TODO Extract to Color Objects
-        self.pageTitles = NSArray(objects: "Red", "Blue", "Green", "Yellow", "Black")
-        self.colors = [UIColor.redColor(), UIColor.blueColor(), UIColor.greenColor(), UIColor.yellowColor(), UIColor.blackColor()]
-        
-        self.pageViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ColorPageViewController") as! UIPageViewController
-        
-        self.pageViewController.dataSource = self
-        
-        self.initStartView()
-        
-        self.pageViewController.view.frame = CGRectMake(0, 30, self.view.frame.width, self.view.frame.size.height - 60)
-        
-        self.addChildViewController(self.pageViewController)
-        self.view.addSubview(self.pageViewController.view)
-        self.pageViewController.didMoveToParentViewController(self)
+
+        // Do any additional setup after loading the view.
     }
-    
-    func initStartView() {
-        let startVc = self.viewControllerAtIndex(0) as ColorViewController
-        let viewControllers = NSArray(object: startVc)
-        
-        self.pageViewController.setViewControllers(viewControllers as? [UIViewController], direction: .Forward, animated: true, completion: nil)
-    }
-    
-    override func  didReceiveMemoryWarning() {
+
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func startColors(sender: AnyObject) {
-        self.initStartView()
-    }
-    
-    func viewControllerAtIndex(index: Int) -> ColorViewController {
-        if (self.pageTitles.count == 0 || index >= self.pageTitles.count) {
-            return ColorViewController()
-        }
-        
-        let vc = self.storyboard?.instantiateViewControllerWithIdentifier("ColorViewController") as! ColorViewController
-        
-        vc.titleText = self.pageTitles[index] as! String
-        vc.pageIndex = index
-        vc.view.backgroundColor = self.colors[index]
-        
-        return vc
-    }
 
-    // MARK: - Page View Controller Data Source
-    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+    /*
+    // MARK: - Navigation
 
-        let vc = viewController as! ColorViewController
-        var index = vc.pageIndex as Int
-        
-        if (index == 0 || index == NSNotFound) {
-            return nil
-        }
-        
-        --index
-        return self.viewControllerAtIndex(index)
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
-    
-    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
-        
-        let vc = viewController as! ColorViewController
-        var index = vc.pageIndex as Int
-        
-        if (index == NSNotFound) {
-            return nil
-        }
-        
-        ++index
-        
-        if (index == self.pageTitles.count) {
-            return nil
-        }
-        
-        return self.viewControllerAtIndex(index)
-    }
-    
-    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
-        return self.pageTitles.count
-    }
-    
-    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
-        return 0
-    }
-    
+    */
+
 }
